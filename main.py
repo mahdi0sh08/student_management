097617209469647cb856c1ff1fecbb29396b0b6b
mahdi <shm703586@gmail.com>
2024-05-08 15:52:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class StudentManagementApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Student Management System')
-        self.geometry('250x400')
+        self.geometry('500x800')
 
 
 if __name__ == '__main__':
