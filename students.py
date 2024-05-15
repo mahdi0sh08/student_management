4f48d764b661bb641f1cfd27daa2b7bb3dcfc186
mahdi <shm703586@gmail.com>
2024-05-15 17:11:33 +0330

    Create students.py

---
+++ students.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self,meli,first_name,last_name,age,email):
+        self.meli=meli
+        self.first_name=first_name
+        self.last_name =last_name
+        self.age=age
+        self.email=email
